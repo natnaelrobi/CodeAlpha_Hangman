b6319173a9c9b18320f9ab050ba15a5129f7e936
Natnael Worku <natnaelworku18@gmail.com>
2026-08-15 11:39:47 +0300

    Delete sentencehyphen.py

--- sentencehyphen.py
+++
@@ -1,8 +0,0 @@
-sentence = input("Please enter a sentence:\n")
-sentence = sentence.replace(" ","-")
-print(sentence)
-#splitting the sentence into words
-#words = sentence.split()
-#joining each words using hyphen
-#hyphenated_sentence = "-".join(words)
-#print(hyphenated_sentence)
