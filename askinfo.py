2ce0e9a97b14182bab629c31ddb0311fcb87d282
Natnael Worku <natnaelworku18@gmail.com>
2026-08-15 11:37:58 +0300

    Delete askinfo.py

--- askinfo.py
+++
@@ -1,4 +0,0 @@
-name = input("What is you name? ")
-age = input("How old are you? ")
-favorite_number = input("What is your favorite number? ")
-print(f"Hello, {name}. You are {age} years old. Your favorite number is {favorite_number}")
