29bd8ef61ec650be6c6a9961db86509dc7277178
Natnael Worku <natnaelworku18@gmail.com>
2026-08-15 11:39:27 +0300

    Delete namecheck.py

--- namecheck.py
+++
@@ -1,8 +0,0 @@
-name = input("Enter your name: ")
-sent = input("Enter a sentence with your name in it: ")
-sent = sent.lower()
-words = sent.split()
-if name.lower() in words:
-    print("Your name is in the sentence!")
-else:
-    print("Your name is not in the sentence!")
